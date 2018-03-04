5541e47ec85357c88bced6273007bdef2e736d86
Daniel Hernandez <krewx8@gmail.com>
2018-03-04 08:42:17 -0600

    Componente de grafica de Pie.

---
+++ components/PieChartComponent.vue
@@ -0,0 +1,45 @@
+<template>
+  <canvas id="myChart" width="400" height="400"></canvas>
+</template>
+
+<script>
+export default {
+  name:'PieChartComponent',
+  props:{
+    data:{
+      type:Array,
+      required:false
+    },
+    labels: {
+      type:Array,
+      required:false
+    },
+    label: {
+      type:String,
+      required:false
+    }
+  },
+  mounted: function(){
+  	var ctx = document.getElementById("myChart");
+  	var myChart = new Chart(ctx, {
+  	    type: 'pie',
+  	    data: {
+  	        labels: (this.labels) ? this.labels : ["Red", "Blue"],
+  	        datasets: [{
+  	            label: (this.label) ? this.label : '# of Votes',
+  	            data: (this.data) ? this.data : [20, 80],
+  	            backgroundColor: [
+  	                'rgba(255, 99, 132, 0.2)',
+  	                'rgba(54, 162, 235, 0.2)'
+  	            ],
+  	            borderWidth: 1
+  	        }]
+  	    },
+  	    options: {}
+  	});
+  }
+}
+</script>
+
+<style>
+</style>
--- pages/paciente/medicamentos/_id/index.vue
+++ pages/paciente/medicamentos/_id/index.vue
@@ -1,14 +1,18 @@
 <template id="">
   <div class="">
     <MedicamentoDetalle />
+    <PieChartComponent>
+    </PieChartComponent>
   </div>
 </template>
 
 <script type="text/javascript">
 import  MedicamentoDetalle from '~/components/Medicamentos/MedicamentoDetalle.vue'
+import  PieChartComponent from '~/components/PieChartComponent.vue'
   export default {
     components:{
-      MedicamentoDetalle
+      MedicamentoDetalle,
+      PieChartComponent
     }
   }
 </script>
